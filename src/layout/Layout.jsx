373ad492ea4a2801b17d5316eaeda6908e137743
Ha Nguyen Thao Vy <112704960+jjoevv@users.noreply.github.com>
2024-03-31 21:45:29 +0700

    fix

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -5,17 +5,11 @@ import Logo from './../assets/imgs/logo.png'
 const Layout = () => {
   const location = useLocation()
   
-  const ScrollToTop = () => {
-    window.scrollTo(0, 0);
-    return null;
-  };
   return (
     <>
-    
-    <ScrollToTop/>
      <Navbar collapseOnSelect expand="lg" className='blue-normal p-0 w-100 shadow' fixed='top'>
   <Container fluid className='d-flex justify-content-between mx-5 px-2 p-0'>
-    <Navbar.Brand href="/" className='ms-4 py-2 fs-large text-primary'>
+    <Navbar.Brand as={Link} to="/" className='ms-4 py-2 fs-large text-primary'>
       <Image src={Logo} width={60}/>
     </Navbar.Brand>
     <Navbar.Toggle aria-controls="responsive-navbar-nav" className='toggle-icon'/>
