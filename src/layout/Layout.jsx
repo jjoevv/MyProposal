4a042014f8913ab01d702af19241005ae45f24ec
Ha Nguyen Thao Vy <112704960+jjoevv@users.noreply.github.com>
2024-03-31 21:59:01 +0700

    fix scroll

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -4,9 +4,13 @@ import { Link, Outlet, useLocation } from 'react-router-dom'
 import Logo from './../assets/imgs/logo.png'
 const Layout = () => {
   const location = useLocation()
-  
+  const ScrollToTop = () => {
+    window.scrollTo(0, 0);
+    return null;
+  };
   return (
     <>
+    <ScrollToTop/>
      <Navbar collapseOnSelect expand="lg" className='blue-normal p-0 w-100 shadow' fixed='top'>
   <Container fluid className='d-flex justify-content-between mx-5 px-2 p-0'>
     <Link to="/" className='ms-5 py-2 fs-large text-primary'>
