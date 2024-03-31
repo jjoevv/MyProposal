3e2d7aa7770aec3d303857f8da0dcf596ce23d0f
Ha Nguyen Thao Vy <112704960+jjoevv@users.noreply.github.com>
2024-03-31 14:10:39 +0700

    update url

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/MyProposal/",
+  base: "/MyProposal/hntv/",
   plugins: [react()],
 })
